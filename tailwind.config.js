f3cfa39dc101157bd373c6500a997f1967be6c70
trlawton <rlawton.photo@gmail.com>
2023-08-13 22:55:02 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#079211',
+        'white': '#FFFFF',
+        'light-gray': '#D1D5DB',
+        'gray': '#808080',
+        'deep-gray': '#0A0A0F',
+        'light-dark': '#313131',
+        'dark': '#101017',
+        'deep-dark': '#050507',
+      }
+    },
+  },
+  plugins: [],
+}
